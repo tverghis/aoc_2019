4381a6f1b261812fed372c6aecf6fbbd22c561ef
Tarun Verghis <tarun.verghis@gmail.com>
2019-12-01 20:06:18 -0800

    Day 1 Part 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt").expect("Could not read from file");
+
+    let sum = input.lines().fold(0, |acc, x| {
+        acc + mass_to_fuel(x.parse::<i32>().expect("Could not convert input to i32"))
+    });
+    dbg!(sum);
+}
+
+fn mass_to_fuel(mass: i32) -> i32 {
+    mass / 3 - 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mass_to_fuel() {
+        assert_eq!(mass_to_fuel(12), 2);
+        assert_eq!(mass_to_fuel(14), 2);
+        assert_eq!(mass_to_fuel(1969), 654);
+        assert_eq!(mass_to_fuel(100756), 33583);
+    }
 }
