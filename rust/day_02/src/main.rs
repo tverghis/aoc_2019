ba0a36df43fc1c6e00bbf01f967c298747462c0e
Tarun Verghis <tarun.verghis@gmail.com>
2019-12-02 18:43:07 -0800

    Day 2 Part 2 (Rust)

--- rust/day_02/src/main.rs
+++ rust/day_02/src/main.rs
@@ -19,18 +19,43 @@ impl From<usize> for OpCode {
 
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Cannot open input file");
-    let mut instr_arr: Vec<usize> = input
+    let instr_arr: Vec<usize> = input
         .trim()
         .split(',')
         .map(|i| i.parse().expect("Could not parse into i8"))
         .collect();
 
+    println!("Part 1: {}", part_1(instr_arr.clone()));
+    println!(
+        "Part 2: {}",
+        part_2(instr_arr.clone()).expect("Did not find a suitable combination")
+    );
+}
+
+fn part_1(mut instr_arr: Vec<usize>) -> usize {
     instr_arr[1] = 12;
     instr_arr[2] = 2;
 
     run_instructions(&mut instr_arr);
 
-    println!("Part 1: {}", instr_arr[0]);
+    instr_arr[0]
+}
+
+fn part_2(instr_arr: Vec<usize>) -> Option<usize> {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            let mut tmp_instr_arr = instr_arr.clone();
+            tmp_instr_arr[1] = noun;
+            tmp_instr_arr[2] = verb;
+
+            run_instructions(&mut tmp_instr_arr);
+            if tmp_instr_arr[0] == 19_690_720 {
+                return Some(100 * noun + verb);
+            }
+        }
+    }
+
+    None
 }
 
 fn run_instructions(instr_arr: &mut [usize]) {
