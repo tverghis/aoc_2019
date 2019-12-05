513469b5a53a25e4a8ab79ee14fbf7dd0819d0af
Tarun Verghis <tarun.verghis@gmail.com>
2019-12-05 00:07:19 -0800

    Day 4 Part 2 (Rust)

--- rust/day_04/src/main.rs
+++ rust/day_04/src/main.rs
@@ -1,8 +1,12 @@
+/// Ugly solutions, but I tried to do this without converting to Strings.
+
+#[allow(clippy::unreadable_literal)]
+
 fn main() {
-    println!("Part 1: {}", part_1(353096..=843212));
+    println!("Part 1: {}", solution(353096..=843212));
 }
 
-fn part_1(range: std::ops::RangeInclusive<u32>) -> usize {
+fn solution(range: std::ops::RangeInclusive<u32>) -> usize {
     range.filter(|&n| is_valid_password(n)).count()
 }
 
@@ -12,16 +16,29 @@ fn is_valid_password(n: u32) -> bool {
 
 fn has_two_adjacent_digits(n: u32) -> bool {
     let mut n = n;
+    let mut last_matched_digit = None;
+    let mut is_valid = false;
 
     for _ in 0..5 {
         if n % 10 == (n % 100) / 10 {
-            return true;
+            if let Some(p) = last_matched_digit {
+                is_valid = p != (n % 10);
+            } else {
+                is_valid = true;
+            }
+
+            last_matched_digit = Some(n % 10);
+        } else {
+            if is_valid {
+                return true;
+            }
+            last_matched_digit = None;
         }
 
-        n = n / 10;
+        n /= 10;
     }
 
-    false
+    is_valid
 }
 
 fn has_increasing_digits(n: u32) -> bool {
@@ -32,7 +49,7 @@ fn has_increasing_digits(n: u32) -> bool {
             return false;
         }
 
-        n = n / 10;
+        n /= 10;
     }
 
     true
@@ -44,10 +61,13 @@ mod test {
 
     #[test]
     fn test_adjacent_digits() {
-        assert!(has_two_adjacent_digits(111111));
         assert!(has_two_adjacent_digits(223450));
         assert!(has_two_adjacent_digits(133456));
+        assert!(has_two_adjacent_digits(112233));
+        assert!(has_two_adjacent_digits(111122));
+        assert!(!has_two_adjacent_digits(123444));
         assert!(!has_two_adjacent_digits(123789));
+        assert!(!has_two_adjacent_digits(111111));
     }
 
     #[test]
@@ -59,7 +79,7 @@ mod test {
 
     #[test]
     fn test_valid_password() {
-        assert!(is_valid_password(111111));
+        assert!(!is_valid_password(111111));
         assert!(!is_valid_password(223450));
         assert!(!is_valid_password(123789));
     }
